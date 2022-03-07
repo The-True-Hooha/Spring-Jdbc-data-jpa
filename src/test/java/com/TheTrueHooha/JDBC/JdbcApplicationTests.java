f7468ab73de41ee9b7f1929f5765b11f33899cd6
David Ogar <owogogahhero@outlook.com>
2022-03-07 12:15:14 +0100

    made master the default branch

---
+++ src/test/java/com/TheTrueHooha/JDBC/JdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TheTrueHooha.JDBC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
